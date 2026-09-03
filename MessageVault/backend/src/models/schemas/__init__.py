1391f1ac3bed3b5dc36927dafb0bd8682e998198
SashaPortnov <portnsawa2@gmail.com>
2026-09-03 20:30:47 +0300

    fix: deleting the MessageVault folder

--- MessageVault/backend/src/models/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import Base
-from .message import Message
-
-__all__ = ["Base", "Message"]
--- MessageVault/backend/src/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from .message import router as messageRouter
-
-router = APIRouter()
-router.include_router(messageRouter, prefix="/messages", tags=["Messages"])
